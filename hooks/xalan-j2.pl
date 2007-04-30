c1d441844e5431e5a78d88c1c1e8d68a99da32c0
Igor Vlasenko <viy@altlinux.org>
2007-04-30 15:34:34 +0300

    simple versioning

--- hooks/xalan-j2.pl
+++ hooks/xalan-j2.pl
@@ -31,5 +31,5 @@ $spechook = sub {
     # hack around broken deps
     $jpp->get_section('package','')->subst(qr'BuildRequires:\s*java-devel','##BuildRequires: java-devel');
     # 1.5 hack
-    $jpp->get_section('package','')->subst(qr'BuildRequires:\s*jpackage-1.4-compat','BuildRequires: jpackage-1.5-compat');
+#    $jpp->get_section('package','')->subst(qr'BuildRequires:\s*jpackage-1.4-compat','BuildRequires: jpackage-1.5-compat');
 }
