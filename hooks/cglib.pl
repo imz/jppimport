5e317b94da8ca7adc0a1233c241671e2ceb0f259
Igor Vlasenko <viy@altlinux.org>
2007-05-30 11:20:54 +0300

    fix in cglib hook

--- hooks/cglib.pl
+++ hooks/cglib.pl
@@ -2,10 +2,12 @@
 
 $spechook = \&set_bootstrap_nohook;
 
+# report design of cglib-hook as bug
+
 sub set_bootstrap_nohook {
     my ($jpp, $alt) = @_;
     $jpp->get_section('package','')->unshift_body('%define _without_hook 1'."\n");
-    $jpp->get_section('package','nohook')->push_body('Provides: cglib'."\n");
+    $jpp->get_section('package','nohook')->push_body('Provides: cglib = 0:%{version}'."\n");
     $jpp->get_section('install')->push_body('ln -s %{name}-nohook.jar $RPM_BUILD_ROOT%{_javadir}/%{name}.jar'."\n");
     $jpp->get_section('files','nohook')->push_body('%{_javadir}/%{name}.jar'."\n");
     $jpp->set_changelog('- imported with jppimport script; note: bootstrapped version');
