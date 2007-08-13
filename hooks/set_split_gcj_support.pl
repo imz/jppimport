8c3d5423b81a096a56e8c0d40f5fabbdea2a4316
Igor Vlasenko <viy@altlinux.org>
2007-08-13 09:38:52 +0300

    misc hooks
    skip maven-plugin-itest (looks obsolete)

--- hooks/set_split_gcj_support.pl
+++ hooks/set_split_gcj_support.pl
@@ -6,6 +6,9 @@ sub set_split_gcj_support {
     my ($jpp, $alt) = @_;
     $jpp->get_section('package','')->subst(qr'(?<=\?_without_gcj)\s$','');
     $jpp->get_section('package','')->subst(qr'(?<=\?_gcj_sup)\s$','');
+    $jpp->get_section('package','')->subst(qr'(?<=_gcj_support}}%{!\?_g)\s$','');
+
+#_gcj_support}}%{!?_g
 #%define gcj_support %{?_with_gcj_support:1}%{!?_with_gcj_support:%{?_without_gcj
 #_support:0}%{!?_without_gcj_support:%{?_gcj_support:%{_gcj_support}}%{!?_gcj_sup
 }
--- hooks/struts.pl
+++ hooks/struts.pl
@@ -1,2 +1,7 @@
 require 'set_add_jspapi_dep.pl';
 require 'set_with_coreonly.pl';
+
+$spechook = sub {
+    my ($jpp, $alt) = @_;
+    $jpp->disable_package('webapps-tomcat3');
+}
