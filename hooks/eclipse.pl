e9d35e2a687d5b4a142901d4990323d31427837a
Igor Vlasenko <viy@altlinux.org>
2007-11-08 09:39:59 +0200

    fixes in process of eclipse build

--- hooks/eclipse.pl
+++ hooks/eclipse.pl
@@ -1,19 +1,34 @@
 #!/usr/bin/perl -w
 
 require 'set_target_14.pl';
+require 'set_update_menus.pl';
 
 $spechook = sub {
     my ($jpp, $alt) = @_;
     $jpp->get_section('package','')->unshift_body('BuildRequires: java-javadoc'."\n");
-    $jpp->get_section('package','')->subst(qr'BuildRequires: icu4j-eclipse', '#BuildRequires: icu4j-eclipse');
+    #Epoch:  1
+    $jpp->get_section('package','')->subst(qr'Epoch:\s+1', 'Epoch:  0');
+    $jpp->get_section('package','')->subst(qr'^BuildRequires: icu4j-eclipse', '#BuildRequires: icu4j-eclipse');
+    $jpp->get_section('package','rcp')->subst(qr'^Requires: icu4j-eclipse', '#Requires: icu4j-eclipse');
     $jpp->get_section('package','ecj')->subst(qr'Obsoletes:\s*ecj', '#Obsoletes:	ecj');
     $jpp->get_section('package','ecj')->subst(qr'Provides:\s*ecj', '#Provides:	ecj');
+    $jpp->get_section('package','')->subst(qr'%{name}-fedora-splash-3.3.0.png', '%{name}-altlinux-splash-3.3.0.png');
+    $jpp->copy_to_sources('eclipse-altlinux-splash-3.3.0.png');
+
     # bootstrap hack around icu4j w/o eclipse
     $jpp->get_section('prep')->subst(qr'rm plugins/com.ibm.icu_3.6.1.v20070417.jar', 'mv plugins/com.ibm.icu_3.6.1.v20070417.jar plugins/com.ibm.icu_3.6.1.v20070417.jar.no');
     $jpp->get_section('prep')->push_body('
 rm plugins/com.ibm.icu_3.6.1.v20070417.jar
 mv plugins/com.ibm.icu_3.6.1.v20070417.jar.no plugins/com.ibm.icu_3.6.1.v20070417.jar
 ');
+
+    # multilib_support temporally disabled due to failed build
+    $jpp->get_section('package','')->unshift_body('%def_disable multilib_support'."\n");
+    $jpp->get_section('install')->unshift_body_before(q{%if_enabled multilib_support
+}, qr'# Ensure that the zip files are the same across all builds.');
+    $jpp->get_section('install')->unshift_body_after(q{%endif # multilib_support
+}, qr'rm -rf \${RPM_BUILD_ROOT}/tmp');
+
 
     # they loose JAVA_HOME :(
     $jpp->get_section('prep')->unshift_body_after(q{
--- hooks/log4j.pl
+++ hooks/log4j.pl
@@ -1,12 +1,9 @@
 require 'set_sasl_hook.pl';
 require 'set_manual_no_dereference.pl';
+require 'set_update_menus.pl';
 require 'add_missingok_config.pl';
 
 $spechook = sub {
     my ($jpp, $alt) = @_;
     &add_missingok_config($jpp,'/etc/chainsaw.conf');
-    $jpp->get_section('post','')->push_body(q'%update_menus
-');
-    $jpp->get_section('postun','')->push_body(q'%clean_menus
-');
 }
