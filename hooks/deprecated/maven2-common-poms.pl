5f172fc65b14f136a635c38720623fd39710db36
Igor Vlasenko <viy@altlinux.org>
2007-11-14 00:10:25 +0200

    first maven2 that works

---
+++ hooks/deprecated/maven2-common-poms.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+
+$spechook = sub {
+    my ($jpp, $alt) = @_;
+    $jpp->get_section('package','')->unshift_body('BuildRequires: cglib'."\n");
+    $jpp->get_section('build')->push_body(q{
+cat > JPP-cglib-full.pom <<EOF
+<project>
+  <modelVersion>4.0.0</modelVersion>
+      <groupId>cglib</groupId>
+      <artifactId>cglib-full</artifactId>
+      <version>2.0</version>
+</project>
+EOF
+});
+}
+
+
+__END__
+                <maven>
+                        <groupId>cglib</groupId>
+                        <artifactId>cglib-full</artifactId>
+                        <version>2.0</version>
+                </maven>
+                <jpp>
+                        <groupId>JPP</groupId>
+                        <artifactId>cglib</artifactId>
+                        <version>2.0</version>
+                </jpp>
+ 
