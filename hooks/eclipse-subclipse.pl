f0f65fc277480fb29a391d8c614ab5afe9507954
Igor Vlasenko <viy@altlinux.org>
2008-07-28 20:13:43 +0300

    modified:   eclipse-subclipse.pl

--- hooks/eclipse-subclipse.pl
+++ hooks/eclipse-subclipse.pl
@@ -3,7 +3,8 @@
 
 push @SPECHOOKS, sub {
     my ($jpp, $alt) = @_;
-    $jpp->get_section('prep')->subst(qr'%{_libdir}/svn-javahl/svn-javahl.jar','/usr/share/java/svn-javahl.jar');
-    $jpp->get_section('install')->subst(qr'%{_libdir}/svn-javahl/svn-javahl.jar','/usr/share/java/svn-javahl.jar');
+    $jpp->get_section('package','')->subst(qr'^%define javahl_dir .*','%define javahl_dir %{_javadir}'."\n");
+#    $jpp->get_section('prep')->subst(qr'%{_libdir}/svn-javahl/svn-javahl.jar','/usr/share/java/svn-javahl.jar');
+#    $jpp->get_section('install')->subst(qr'%{_libdir}/svn-javahl/svn-javahl.jar','/usr/share/java/svn-javahl.jar');
 };
 
