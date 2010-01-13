19298f3dfeee82a7e558a3b94992e353f5344ded
Igor Vlasenko <viy@altlinux.org>
2010-01-13 20:09:45 +0200

    modified:   jppimport

--- hooks/db-ojb.pl
+++ hooks/db-ojb.pl
@@ -5,16 +5,7 @@ sub {
     my ($jpp, $alt) = @_;
     $jpp->get_section('package','')->unshift_body('BuildRequires: xdoclet'."\n");
     $jpp->get_section('build')->unshift_body('export ANT_OPTS="$ANT_OPTS -Xms512m -Xmx2048m -Xss1m"'."\n");
+
+    #$jpp->get_section('package','')->subst('village','db-torque-village');
 }
 __END__
-note: ojb does not support new db-torque 3.3, but there is no new releases yet :(
-189a190,194
-> pushd lib
-> mv torque-gen-3.1.1.jar.no db-torque-gen.jar
-> popd
-> 
-222c227
-< ln -sf $(build-classpath db-torque-gen) .
----
-> #ln -sf $(build-classpath db-torque-gen) .
-
