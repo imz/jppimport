30be852c691a1e4e9f0a03ae8a82693e23cac6ed
Igor Vlasenko <viy@altlinux.org>
2011-09-02 22:45:37 +0300

    bugfix

--- hooks/mysql-connector-java.pl
+++ hooks/mysql-connector-java.pl
@@ -22,12 +22,11 @@ popd
 find %buildroot%_datadir -name README.txt -delete
 ');
 
-    # E: Версия >='0:1.0.1-0.a.1' для 'jta' не найдена
-    $jpp->get_section('package','')->subst_if(qr'0:1.0.1-0.a.1','0:1.0.1',qr'Requires:');
-
 };
 
 __END__
+    # E: Версия >='0:1.0.1-0.a.1' для 'jta' не найдена
+    $jpp->get_section('package','')->subst_if(qr'0:1.0.1-0.a.1','0:1.0.1',qr'Requires:');
 
     $jpp->get_section('package','')->subst('BuildRequires: jboss','#BuildRequires: jboss');
     ######## jboss ################
