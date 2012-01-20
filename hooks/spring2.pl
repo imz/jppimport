3355414b523db2139c149164cc1ff2e672f27c41
Igor Vlasenko <viy@altlinux.org>
2012-01-20 21:35:19 +0200

    modified:   deps.notes

--- hooks/spring2.pl
+++ hooks/spring2.pl
@@ -4,9 +4,48 @@ require 'windows-thumbnail-database-in-package.pl';
 
 push @SPECHOOKS, sub {
     my ($jpp, $alt) = @_;
+# do we need it? inherited, but not tested
     $jpp->get_section('package','')->unshift_body('BuildRequires: avalon-framework'."\n");
 };
 __END__
+current - fixes for old slf4j and old tiles
+
+--- spring2.spec.0	2012-01-20 17:53:05.042587824 +0000
++++ spring2.spec	2012-01-20 19:07:39.150562692 +0000
+@@ -1149,9 +1149,9 @@
+ # BUILD/spring/lib/serp/serp-1.13.1.jar.no
+ ln -sf $(build-classpath serp) lib/serp/serp-1.13.1.jar
+ # BUILD/spring/lib/slf4j/slf4j-api-1.5.0.jar.no
+-ln -sf $(build-classpath slf4j/slf4j-api) lib/slf4j/slf4j-api-1.5.0.jar
++ln -sf $(build-classpath slf4j/api) lib/slf4j/slf4j-api-1.5.0.jar
+ # BUILD/spring/lib/slf4j/slf4j-log4j12-1.5.0.jar.no
+-ln -sf $(build-classpath slf4j/slf4j-log4j12) lib/slf4j/slf4j-log4j12-1.5.0.jar
++ln -sf $(build-classpath slf4j/log4j12) lib/slf4j/slf4j-log4j12-1.5.0.jar
+ # BUILD/spring/lib/struts/struts.jar.no
+ ln -sf $(build-classpath struts) lib/struts/struts.jar
+ ln -sf $(build-classpath struts-tiles) lib/struts/struts-tiles.jar
+@@ -1161,13 +1161,13 @@
+ ln -sf $(build-classpath jcommander) lib/testng/jcommander.jar
+ ln -sf $(build-classpath objenesis) lib/testng/objenesis.jar
+ # BUILD/spring/lib/tiles/tiles-api-2.0.6.jar.no
+-ln -sf $(build-classpath tiles/api) lib/tiles/tiles-api-2.0.6.jar
++ln -sf $(build-classpath tiles/api) lib/tiles/api-2.0.6.jar
+ # BUILD/spring/lib/tiles/tiles-core-2.0.6.jar.no
+-ln -sf $(build-classpath tiles/core) lib/tiles/tiles-core-2.0.6.jar
++ln -sf $(build-classpath tiles/core) lib/tiles/core-2.0.6.jar
+ # BUILD/spring/lib/tiles/tiles-jsp-2.0.6.jar.no
+-ln -sf $(build-classpath tiles/jsp) lib/tiles/tiles-jsp-2.0.6.jar
++ln -sf $(build-classpath tiles/jsp) lib/tiles/jsp-2.0.6.jar
+ #
+-ln -sf $(build-classpath tiles/servlet) lib/tiles/tiles-servlet-2.0.6.jar
++#ln -sf $(build-classpath tiles/servlet) lib/tiles/tiles-servlet-2.0.6.jar
+ # BUILD/spring/lib/tomcat/catalina.jar.no
+ ln -sf $(build-classpath tomcat5/catalina) lib/tomcat/catalina.jar
+ # BUILD/spring/lib/tomcat/naming-resources.jar.no
+
+
+
+___OOOOLD_HACKS__
 hack; see
 http://jira.springframework.org/browse/SPR-5145
 ALSO
