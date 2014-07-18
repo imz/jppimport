8121ee1a1701300af3eb1bb2fdcbb043a1dd4634
Igor Vlasenko <viy@altlinux.org>
2014-07-19 00:30:20 +0300

    new hooks

--- hooks/maven-ant-tasks.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl -w
-
-push @SPECHOOKS, sub {
-    my ($jpp, $alt) = @_;
-    $jpp->get_section('package')->unshift_body('BuildRequires: maven-error-diagnostics maven-dependency-plugin'."\n");
-}
-
-__END__
