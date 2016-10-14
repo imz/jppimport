be8ccf567d0e33575d9d2bd5bcbac301ef2f763c
Igor Vlasenko <viy@altlinux.org>
2016-10-14 13:13:09 +0300

    added patch

--- hooks/pki-core.pl
+++ hooks/pki-core.pl
@@ -4,7 +4,12 @@ push @SPECHOOKS,
 sub {
     my ($spec, $parent) = @_;
 
+    # Mikhail Efremov to Igor
+    # Hello!
+    # Возможно проблема в python-module-requests, но без этого патча pki-core
+    # был не рабочий совсем. (for pki-core-10.2.6)
     $spec->add_patch('pki-core-alt-local-urllib3.patch',STRIP=>1);
+
     $spec->get_section('pretrans','-n pki-base')->delete;
     $spec->get_section('files','-n pki-base')->push_body('%dir %{_datadir}/pki/key'."\n");
     foreach my $pkg (qw/pki-base pki-server/) {
