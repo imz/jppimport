5c78c4db62ff0b2736cf9f9a6a866649e4f17a65
Igor Vlasenko <viy@altlinux.org>
2016-10-14 15:14:56 +0300

    cas@ jogl2 patch

---
+++ hooks/jogl2.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+
+push @SPECHOOKS, 
+sub {
+    my ($spec, $parent) = @_;
+    # bad elf symbols - native broadcom
+    $spec->add_patch('jogl2-disable-build-native-broadcom.patch',STRIP=>2);
+    $spec->get_section('package','')->push_body('BuildArch: noarch'."\n");
+    $spec->get_section('install')->exclude_body(qr'^install .*build/lib/\*\.so');
+    $spec->get_section('install')->subst_body(qr'_jnidir','_javadir');
+    $spec->get_section('files','')->exclude_body('_libdir');
+};
+
+__END__
