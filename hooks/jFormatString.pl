c31facd781f48b5a6ae775c87aeb2eaa2646a0aa
Igor Vlasenko <viy@altlinux.org>
2016-11-30 00:19:45 +0200

    new hooks

--- hooks/jFormatString.pl
+++ hooks/jFormatString.pl
@@ -7,6 +7,7 @@ sub {
 Obsoletes: jformatstring <= 0:0-alt1_0.3.20081016svnjpp6
 Conflicts: jformatstring <= 0:0-alt1_0.3.20081016svnjpp6
 '."\n");
+    $spec->get_section('pretrans','javadoc')->delete;
 };
 
 __END__
--- hooks/regexp.pl
+++ hooks/regexp.pl
@@ -8,4 +8,5 @@ sub {
     $spec->get_section('package','')->unshift_body('Obsoletes: jakarta-regexp = 1.4-alt2'."\n");
     $spec->get_section('package','')->unshift_body('Obsoletes: jakarta-regexp = 1.4-alt1'."\n");
     $spec->get_section('package','')->push_body('Provides: jakarta-regexp = %{version}-%{release}'."\n");
+    $spec->get_section('pretrans','javadoc')->delete;
 }
